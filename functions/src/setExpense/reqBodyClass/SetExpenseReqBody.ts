8554ba8c44ecb38b6b58783a32d3c943f090e2e9
SaoodCS <saood-aslam@outlook.com>
2024-02-21 19:46:45 +0000

    change paymentType field to hasDistInstruction field

--- functions/src/setExpense/reqBodyClass/SetExpenseReqBody.ts
+++ functions/src/setExpense/reqBodyClass/SetExpenseReqBody.ts
@@ -3,20 +3,20 @@ export interface ISetExpenseReqBody {
    expenseValue: number;
    expenseType: string;
    paused: string;
-   paymentType: string;
+   hasDistInstruction: string;
    id?: number;
 }
 
 export default class SetExpenseReqBody {
    static isValid(body: unknown): body is ISetExpenseReqBody {
-      const { expenseName, expenseValue, expenseType, paused, paymentType } =
+      const { expenseName, expenseValue, expenseType, paused, hasDistInstruction } =
          body as ISetExpenseReqBody;
       return (
          typeof expenseName === 'string' &&
          typeof expenseValue === 'number' &&
          typeof expenseType === 'string' &&
          typeof paused === 'string' &&
-         typeof paymentType === 'string'
+         typeof hasDistInstruction === 'string'
       );
    }
 }
