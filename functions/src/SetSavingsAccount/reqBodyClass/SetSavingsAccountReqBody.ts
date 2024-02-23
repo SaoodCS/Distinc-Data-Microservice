b94597906f22ef93cc7d4fce32f35f1c29c3ac39
SaoodCS <saood-aslam@outlook.com>
2024-02-23 11:11:49 +0000

    update setSavingsAccount schema

--- functions/src/SetSavingsAccount/reqBodyClass/SetSavingsAccountReqBody.ts
+++ functions/src/SetSavingsAccount/reqBodyClass/SetSavingsAccountReqBody.ts
@@ -5,18 +5,20 @@ export interface ISetSavingsAccountReqBody {
    targetToReach: OptionalNumberInput;
    currentBalance: OptionalNumberInput;
    isTracked: 'true' | 'false';
+   coversYearlyExpenses: 'true' | 'false';
    id?: number;
 }
 
 export default class SetSavingsAccountReqBody {
    static isValid(body: unknown): body is ISetSavingsAccountReqBody {
-      const { accountName, targetToReach, currentBalance, isTracked } =
+      const { accountName, targetToReach, currentBalance, isTracked, coversYearlyExpenses } =
          body as ISetSavingsAccountReqBody;
       return (
          typeof accountName === 'string' &&
          (typeof targetToReach === 'number' || targetToReach === '') &&
          (typeof currentBalance === 'number' || currentBalance === '') &&
-         (isTracked === 'true' || isTracked === 'false')
+         (isTracked === 'true' || isTracked === 'false') &&
+         (coversYearlyExpenses === 'true' || coversYearlyExpenses === 'false')
       );
    }
 }
