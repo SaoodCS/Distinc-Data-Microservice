fb7bb036e2a5da86427a43acad59b4a823f59116
saood <saood-aslam@outlook.com>
2025-04-02 15:27:49 +0100

    payment method expense field added

--- functions/src/setExpense/reqBodyClass/SetExpenseReqBody.ts
+++ functions/src/setExpense/reqBodyClass/SetExpenseReqBody.ts
@@ -4,17 +4,19 @@ export interface ISetExpenseReqBody {
    expenseType: string;
    paused: string;
    hasDistInstruction: string;
+   paymentMethod: string;
    id?: number;
 }
 
 export default class SetExpenseReqBody {
    static isValid(body: unknown): body is ISetExpenseReqBody {
-      const { expenseName, expenseValue, expenseType, paused, hasDistInstruction } =
+      const { expenseName, expenseValue, expenseType, paused, hasDistInstruction, paymentMethod } =
          body as ISetExpenseReqBody;
       return (
          typeof expenseName === 'string' &&
          typeof expenseValue === 'number' &&
          typeof expenseType === 'string' &&
+         typeof paymentMethod === 'string' &&
          typeof paused === 'string' &&
          typeof hasDistInstruction === 'string'
       );
