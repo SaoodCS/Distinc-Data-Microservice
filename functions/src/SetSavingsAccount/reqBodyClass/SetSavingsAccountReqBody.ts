bc5e1f78f457c2051164993bd16150563eada091
saood <saood-aslam@outlook.com>
2025-04-04 11:52:38 +0100

    change from acc covering yearly exp to account covering shortfall

--- functions/src/SetSavingsAccount/reqBodyClass/SetSavingsAccountReqBody.ts
+++ functions/src/SetSavingsAccount/reqBodyClass/SetSavingsAccountReqBody.ts
@@ -5,20 +5,20 @@ export interface ISetSavingsAccountReqBody {
    targetToReach: OptionalNumberInput;
    currentBalance: OptionalNumberInput;
    isTracked: 'true' | 'false';
-   coversYearlyExpenses: 'true' | 'false';
+   coversShortfall: 'true' | 'false';
    id?: number;
 }
 
 export default class SetSavingsAccountReqBody {
    static isValid(body: unknown): body is ISetSavingsAccountReqBody {
-      const { accountName, targetToReach, currentBalance, isTracked, coversYearlyExpenses } =
+      const { accountName, targetToReach, currentBalance, isTracked, coversShortfall } =
          body as ISetSavingsAccountReqBody;
       return (
          typeof accountName === 'string' &&
          (typeof targetToReach === 'number' || targetToReach === '') &&
          (typeof currentBalance === 'number' || currentBalance === '') &&
          (isTracked === 'true' || isTracked === 'false') &&
-         (coversYearlyExpenses === 'true' || coversYearlyExpenses === 'false')
+         (coversShortfall === 'true' || coversShortfall === 'false')
       );
    }
 }
